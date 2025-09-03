2a6cdc67605a678f7685f4684d9f5bfb308fae99
lucia-shaper <lucia@shaper.plus>
2025-09-03 10:44:42 -0300

    prueba de cambios

--- src/sections/Why_Exist.jsx
+++ src/sections/Why_Exist.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { AnimatePresence, motion } from "framer-motion";
+import { AnimatePresence } from "framer-motion";
 
 export default function Why_Exist() {
   const [currentText, setCurrentText] = useState("why");
